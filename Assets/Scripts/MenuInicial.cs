95ec11aee23e7873db8013386e0a7ab29006261b
Vanessa <vanessa.ortiz@aluno.ifsp.edu.br>
2024-11-25 22:04:12 -0300

    Segundo commit:
    Neste commit constam os primeiros arquivos.
    
    1 - Criado o Menu Inicial;
    2 - Integrando o Menu Inicial com o Nivel 1 e Créditos;
    3 - Criando a Tela de Créditos;

---
+++ Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    public void iniciarJogo(){
+        SceneManager.LoadScene("Fase 1"); //Aqui definimos qual tela será carregada após apertar o botão de Iniciar
+    }
+
+    public void creditosJogos(){
+        SceneManager.LoadScene("Creditos"); //Aqui definimos qual tela será carregada após apertar o botão de Crédito
+    }
+
+    public void fecharJogo(){
+        Application.Quit();
+        Debug.Log("Fechou o Jogo");
+    }
+}
